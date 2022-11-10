6149c53cf73da1481703085d085da294cb0e0310
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-11-10 12:45:30 +0200

    Created and register models

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from news.models import Category, Article
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_published',
+                    'created_at', 'updated_at', 'views')
+    list_filter = ('author', 'is_published', 'views', 'created_at')
+    ordering = ('is_published', )
+
+
+admin.site.register(Article, ArticleAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category-detail', args=[self.id])
+
+
+class Article(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=150, unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.FileField(upload_to='images/')
+    is_published = models.BooleanField(default=True)
+    views = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news:article', args=[self.title])
