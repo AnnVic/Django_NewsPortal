f48cf671e90f748b6c0a8921a00f69fd2e05519f
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-11-22 09:34:15 +0200

    added urls for login, registration, logout pages

--- news/urls.py
+++ news/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:slug>/', views.article_detail, name='article'),
     path('category/<slug:slug>/', views.category_content, name='category'),
+    path('register', views.register_request, name='register'),
+    path('accounts/login', views.login_request, name='login'),
+    path('accounts/logout', views.logout_request, name='logout'),
 ]
