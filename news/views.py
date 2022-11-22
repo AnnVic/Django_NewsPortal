8a677b0c3a0e6abc20fcba41ef51747d0411bcda
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-11-22 09:32:27 +0200

    added views for login, logout, registration

--- news/views.py
+++ news/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, redirect
 from news.models import Category, Article
-from django.views.generic import ListView, DetailView
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -35,3 +39,47 @@ def article_detail(request, slug):
                'image': article.image,
                }
     return render(request, 'news/article.html', context)
+
+
+def register_request(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('news:home')
+        messages.error(
+            request, 'Unsuccessful registration. Invalid information.')
+    form = NewUserForm()
+    return render(request=request, template_name='news/register.html', context={'register_form': form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}.')
+                return redirect('news:home')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    form = AuthenticationForm()
+    return render(request=request, template_name='news/login.html', context={'login_form': form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'You have successfully logged out.')
+    return redirect('news:home')
+
+
+@login_required
+def profile(request):
+    return render(request, 'news/profile.html')
